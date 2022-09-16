2008154bb555dc3812ff1adb15005d01445c3ae4
msurkhi-1106 <msurkh1@student.ubc.ca>
2022-09-16 12:45:41 -0700

    fixed

--- SortingStrings.java
+++ SortingStrings.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 public class SortingStrings{
     public static void main (String args[]){
         String[] stringArray = {"Liverpool", "Manchester", "Kelowna", "Toronto","Vancouver", "Leeds"};
-        System.out.println(stringstringArray);
         Arrays.sort (stringArray);
         System.out.println(Arrays.toString(stringArray));
     }
