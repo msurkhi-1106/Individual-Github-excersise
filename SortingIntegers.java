5e0a69989259a0298b4f8010fd0b10608acc3e2a
msurkhi-1106 <msurkh1@student.ubc.ca>
2022-09-16 11:30:09 -0700

    Created Array with Random Integers

---
+++ SortingIntegers.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class SortingIntegers {    
+    public static void main(String[] args) {        
+        //define original array  
+        Random rd = new Random();   
+        int [] intArray = new int [5] ;     
+        int temp = 0;    
+ 
+        
+    }    
+}    
