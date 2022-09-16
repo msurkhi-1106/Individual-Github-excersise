abfb5f37a2120f373362c3e16642c755f1a1060e
msurkhi-1106 <msurkh1@student.ubc.ca>
2022-09-16 11:33:11 -0700

    Sorted array

--- SortingIntegers.java
+++ SortingIntegers.java
@@ -13,6 +13,19 @@ public class SortingIntegers {
            System.out.print(intArray[i] + " ");    
         }    
         //Sort the array in ascending order using two for loops    
-        
+        for (int i = 0; i <intArray.length; i++) {     
+          for (int j = i+1; j <intArray.length; j++) {     
+              if(intArray[i] >intArray[j]) {      //swap elements if not in order
+                 temp = intArray[i];    
+                 intArray[i] = intArray[j];    
+                 intArray[j] = temp;    
+               }     
+            }     
+        }    
+        //print sorted array    
+        System.out.println("\nArray sorted in ascending order: ");    
+        for (int i = 0; i <intArray.length; i++) {     
+            System.out.print(intArray[i] + " ");    
+        }    
     }    
 }    
