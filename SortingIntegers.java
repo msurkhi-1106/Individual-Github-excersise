4fe62ff1bcb3a80d2752efba9a52f83ea5dc5064
msurkhi-1106 <msurkh1@student.ubc.ca>
2022-09-16 11:31:02 -0700

    Printed Array with random Integers

--- SortingIntegers.java
+++ SortingIntegers.java
@@ -6,6 +6,13 @@ public class SortingIntegers {
         int [] intArray = new int [5] ;     
         int temp = 0;    
  
+        //print original array    
+       System.out.println("Original array: ");    
+       for (int i = 0; i <intArray.length; i++) { 
+           intArray[i] = rd.nextInt();     
+           System.out.print(intArray[i] + " ");    
+        }    
+        //Sort the array in ascending order using two for loops    
         
     }    
 }    
