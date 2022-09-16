c167e45d0d43d755425ac851855f5f06fc083e92
msurkhi-1106 <msurkh1@student.ubc.ca>
2022-09-16 12:12:47 -0700

    Created Strings array and sorted it

--- SortingStrings.java
+++ SortingStrings.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 public class SortingStrings{
-    public static void main (Strings args[]){
+    public static void main (String args[]){
         String[] stringArray = {"Liverpool", "Manchester", "Kelowna", "Toronto","Vancouver", "Leeds"};
         Arrays.sort (stringArray);
         System.out.println(Arrays.toString(stringArray));
